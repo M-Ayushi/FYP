36f313c1764d6425191e06c9dbb9182908b127d7
Ayushi Mahajan <ayushimahajan05@gmail.com>
2026-04-06 13:23:21 +0100

    alternate fw_invariant lemma introduced

--- FYP/FloydWarshall/CorrectnessTheorem.lean
+++ FYP/FloydWarshall/CorrectnessTheorem.lean
@@ -7,7 +7,7 @@ import FYP.FloydWarshall.Lemmas
 namespace FYP
 
 -- applying fwStep l times is the same as applying fwStep to the list of vertices in l
-lemma fw_invariant {n : ℕ} (G : Graph n) :
+lemma fw_invariant_old {n : ℕ} (G : Graph n) :
   ∀ (l : List (Fin n)) (i j : Fin n),
     (l.foldl fwStep (initDist G)) i j = distUpToList G l i j := by
       intro l
@@ -23,6 +23,30 @@ lemma fw_invariant {n : ℕ} (G : Graph n) :
         simp only [fwStep]
         rw [ih i j, ih i k, ih k j]
         rw [sInf_split G ks k i j]
+
+lemma fw_invariant {n : ℕ} (G : Graph n) :
+  ∀ (l : List (Fin n)) (i j : Fin n),
+    (l.foldl fwStep (initDist G)) i j = distUpToList G l i j := by
+  intro l
+  induction l using List.reverseRecOn with
+  | nil =>
+    apply initDist_eq_sInf
+  | append_singleton ks k ih =>
+    intro i j
+    simp only [List.foldl_append, List.foldl]
+    -- Now LHS is: fwStep (ks.foldl fwStep (initDist G)) k i j
+    -- IH gives:   (ks.foldl fwStep (initDist G)) i j = distUpToList G ks i j
+    -- So we can rewrite the inner matrix using ih
+    rw [show (List.foldl fwStep (initDist G) ks) =
+            (fun i j => distUpToList G ks i j) from funext (fun i => funext (fun j => ih i j))]
+    -- Now LHS is: fwStep (distUpToList G ks) k i j
+    --           = min (distUpToList G ks i j)
+    --                 (distUpToList G ks i k + distUpToList G ks k j)
+    simp only [fwStep]
+    -- RHS: distUpToList G (ks ++ [k]) i j
+    rw [← sInf_split G ks k i j]
+    simp [distUpToList]
+    simp only [or_comm (a := _ = k) (b := _ ∈ ks)]
 
 theorem floydWarshall_correct (G : Graph n) (i j : Fin n) :
   floydWarshall G i j = dist G i j := by
